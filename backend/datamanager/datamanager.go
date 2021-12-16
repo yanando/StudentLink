a5165088b70da8065ba4ce94b78b598b78af7fb2
Yanando <shayan.fakhtei@gmail.com>
2021-12-16 13:22:39 +0100

    datamanager things

--- backend/datamanager/datamanager.go
+++ backend/datamanager/datamanager.go
@@ -4,16 +4,18 @@ import (
 	"database/sql"
 	"errors"
 
+	"golang.org/x/crypto/bcrypt"
 	_ "modernc.org/sqlite"
 )
 
 type Datamanager interface {
-	GetUser(id int) (User, error)
-	UpdateUser(User) error
+	AddUser(*User) error
+	GetUser(id int) (*User, error)
+	UpdateUser(*User) error
 	VerifyAuth(username, password string) (int, error)
 
-	AddChatMessage(User, Message) error
-	CreateChannel(User) (int, error)
+	AddChatMessage(*User, Message) error
+	CreateChannel(*User) (int, error)
 }
 
 type StudentLinkDatabase struct {
@@ -34,9 +36,40 @@ func (s *StudentLinkDatabase) Close() error {
 	return s.db.Close()
 }
 
-// func (s *StudentLinkDatabase) GetUser(id int) (User, error) {
-// 	s.db.QueryRow("SELECT * FROM Users WHERE ID=?", id).Scan()
-// }
+func (s *StudentLinkDatabase) AddUser(user *User, password string) error {
+	hash := hashPassword(password)
+	err := s.db.QueryRow("INSERT INTO users (email, username, firstname, lastname, type, pw_hash) Values ($1, $2, $3, $4, $5, $6)",
+		user.Email, user.Username, user.Firstname, user.Lastname, user.Type, hash).Err()
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *StudentLinkDatabase) GetUser(id int) (*User, error) {
+	var u *User = &User{}
+
+	// todo: Populate scan
+	err := s.db.QueryRow("SELECT * FROM users WHERE id=?", id).Scan()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return u, nil
+}
+
+func (s *StudentLinkDatabase) UpdateUser(user *User) error {
+	err := s.db.QueryRow("UPDATE users SET email = ?", user.Email).Err()
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
 
 func (s *StudentLinkDatabase) VerifyAuth(username, password string) (int, error) {
 	if password == "hallo" {
@@ -44,4 +77,17 @@ func (s *StudentLinkDatabase) VerifyAuth(username, password string) (int, error)
 	}
 
 	return 0, errors.New("poop")
+}
+
+func (s *StudentLinkDatabase) AddChatMessage(user *User, msg Message) error {
+	return nil
+}
+
+func (s *StudentLinkDatabase) CreateChannel(user *User) error {
+	return nil
+}
+
+func hashPassword(plaintext string) string {
+	pw, _ := bcrypt.GenerateFromPassword([]byte(plaintext), bcrypt.DefaultCost)
+	return string(pw)
 }
--- backend/go.mod
+++ backend/go.mod
@@ -11,6 +11,7 @@ require (
 require (
 	github.com/mattn/go-isatty v0.0.12 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20200410134404-eec4a21b6bb0 // indirect
+	golang.org/x/crypto v0.0.0-20211215153901-e495a2d5b3d3 // indirect
 	golang.org/x/sys v0.0.0-20211007075335-d3039528d8ac // indirect
 	modernc.org/libc v1.11.104 // indirect
 	modernc.org/mathutil v1.4.1 // indirect
