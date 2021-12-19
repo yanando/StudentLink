d38eaee6cc0f3aea95e28fe92e717ed1c86cbd21
yanando <shayan.fakhtei@gmail.com>
2021-12-19 15:46:08 +0100

    sync

--- frontend/src/routes/Login.svelte
+++ frontend/src/routes/Login.svelte
@@ -1,14 +1,37 @@
 <script lang="ts">
+    let username: string
+    let password: string
 
+    const submit = async () => {
+        if (!username || !password) {
+            alert('Geen gebruikersnaam of wachtwoord ingevoerd!')
+            return
+        }
+
+        const payload = {
+            username: username,
+            password: password
+        }
+
+        const resp = await fetch('http://localhost:8080/api/login', {
+            method: 'POST',
+            body: JSON.stringify(payload),
+            headers: {
+                'content-type': 'application/json'
+            }
+        })
+
+        console.log(resp)
+    }
 </script>
 
 <div id="wrapper">
     <h1>StudentLink Login</h1>
     <div>
-        <input type="text" name="username" placeholder="Username">
-        <input type="password" name="password" placeholder="Password">
+        <input type="text" name="username" placeholder="Username" bind:value={username}>
+        <input type="password" name="password" placeholder="Password" bind:value={password}>
     </div>
-    <button id="submit">Login</button>
+    <button id="submit" on:click={submit}>Login</button>
 </div>
 
 <style>
