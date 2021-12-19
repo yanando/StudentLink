f0167a71d1f97e7f9dac374f8942d8446c8d6727
yanando <shayan.fakhtei@gmail.com>
2021-12-19 15:08:14 +0100

    sync

--- frontend/src/routes/Home.svelte
+++ frontend/src/routes/Home.svelte
@@ -1,8 +1,19 @@
-<script>
-
+<script lang="ts">
+    
 </script>
 
+<h1 id="title">StudentLink</h1>
 <div id="wrapper">
-    <p>Hello world</p>
+    <a href="/login" id="login-button">LOGIN</a>
 </div>
 
+<style lang="scss">
+    #title {
+        text-align: center;
+    }
+
+    #wrapper {
+        display: flex;
+        justify-content: center;
+    }
+</style>
---
+++ frontend/src/routes/Login.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+
+</script>
+
+<div id="wrapper">
+    <input type="text" name="username" placeholder="Username">
+    <input type="password" name="password" placeholder="Password">
+    <button id="submit">Login</button>
+</div>
