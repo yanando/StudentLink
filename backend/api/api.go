1c86ec449fe6b5a34f7254198da2493a45cc7bb0
yanando <shayan.fakhtei@gmail.com>
2021-12-19 16:07:11 +0100

    sync

--- backend/api/api.go
+++ backend/api/api.go
@@ -19,20 +19,32 @@ func NewAPIServer(dataManager datamanager.Datamanager, sessionManager *SessionMa
 		sessionManager: sessionManager,
 		dataManager:    dataManager,
 		Server: http.Server{
-			Addr: ":5000",
+			Addr: ":8080",
 		},
 	}
 
-	router := mux.NewRouter()
+	router := mux.NewRouter().PathPrefix("/api").Subrouter()
 	router.Use(mux.CORSMethodMiddleware(router))
 	router.Use(func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 			w.Header().Set("Content-Type", "application/json")
+			w.Header().Set("Access-Control-Allow-Origin", "*")
+			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE")
+			w.Header().Set("Access-Control-Allow-Headers", "*")
+
 			next.ServeHTTP(w, req)
 		})
 	})
+	router.Use(func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			if req.Method == http.MethodOptions {
+				w.WriteHeader(200)
+				return
+			}
+		})
+	})
 
-	router.HandleFunc("/login", apiServer.LoginHandler).Methods(http.MethodPost)
+	router.HandleFunc("/login", apiServer.LoginHandler).Methods(http.MethodPost, http.MethodOptions)
 
 	router.HandleFunc("/user", apiServer.GetUserHandler).Methods(http.MethodGet)
 	router.HandleFunc("/user", apiServer.UpdateUserHandler).Methods(http.MethodPatch)
