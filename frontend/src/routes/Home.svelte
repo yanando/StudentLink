101fad074d82aab152812e9254a2f34837cbdb16
einstein8612 <128143@student.hetbaken.nl>
2022-03-27 22:15:03 +0200

    spinner

--- frontend/src/routes/Home.svelte
+++ frontend/src/routes/Home.svelte
@@ -1,19 +1,46 @@
 <script lang="ts">
-    import {link} from 'svelte-spa-router'
+  import { push } from "svelte-spa-router";
+  setTimeout(() => {
+    push("/login");
+  }, 1000);
 </script>
 
-<h1 id="title">StudentLink</h1>
 <div id="wrapper">
-    <a href="/login" id="login-button" use:link>LOGIN</a>
+  <div class="lds-dual-ring" />
 </div>
 
 <style lang="scss">
-    #title {
-        text-align: center;
-    }
+  #wrapper {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100vw;
+    height: 100vh;
+    background-color: #252427;
+  }
 
-    #wrapper {
-        display: flex;
-        justify-content: center;
+  .lds-dual-ring {
+    display: inline-block;
+    width: 80px;
+    height: 80px;
+  }
+  .lds-dual-ring:after {
+    content: " ";
+    display: block;
+    width: 64px;
+    height: 64px;
+    margin: 8px;
+    border-radius: 50%;
+    border: 6px solid #0c7d9d;
+    border-color: #0c7d9d transparent #0c7d9d transparent;
+    animation: lds-dual-ring 1.2s linear infinite;
+  }
+  @keyframes lds-dual-ring {
+    0% {
+      transform: rotate(0deg);
     }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
 </style>
