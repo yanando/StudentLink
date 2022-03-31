a2b3ff2c62990fb24827ae0402fe76e507c319d1
Joey Li <128143@student.hetbaken.nl>
2022-03-31 11:44:24 +0200

    overig

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,4 +1,4 @@
-// export const API_BASE = 'http://localhost:8080'
-export const API_BASE = "http://8c68-84-84-186-177.ngrok.io"
+export const API_BASE = 'http://localhost:8080'
+// export const API_BASE = "http://8c68-84-84-186-177.ngrok.io"
 // export const API_BASE = 'http://162.55.53.87:8080'
 // export const API_BASE = 'https://c1fe-116-203-150-25.ngrok.io'
